b4c3a1f2b830056051f851bc56df8919a0fe9a7b
locht <thanhloclhu@gmail.com>
2020-08-09 21:56:44 +0700

    a

--- CatFlowersv2/settings.py
+++ CatFlowersv2/settings.py
@@ -5,11 +5,11 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-# ALLOWED_HOSTS = [
-#     'localhost',
-#     '127.0.0.1:8000',
-#     'catflowersv2.herokuapp.com',
-# ]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1:8000',
+    'catflowersv2.herokuapp.com',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',

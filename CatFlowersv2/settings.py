0e760f66d624fae63ef7485c447f3154a2a4c704
locht <thanhloclhu@gmail.com>
2020-08-09 23:10:02 +0700

    config static

--- CatFlowersv2/settings.py
+++ CatFlowersv2/settings.py
@@ -5,11 +5,11 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1:8000',
-    'catflowersv2.herokuapp.com',
-]
+# ALLOWED_HOSTS = [
+#     'localhost',
+#     '127.0.0.1:8000',
+#     'catflowersv2.herokuapp.com',
+# ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -70,11 +70,12 @@ USE_TZ = True
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
